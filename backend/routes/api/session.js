79efd7dbcc774b3ac7458cf21d1944fa3b4fcbc9
Ben Durham <oorazugoku@gmail.com>
2022-07-05 15:59:04 -0500

    starting images

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -44,13 +44,13 @@ const validateSignup = [
 ];
 
 
-// Validate login info
+// V or username.'),
+//   check('passworalidate login info
 // const validateLogin = [
 //   check('credential')
 //     .exists({ checkFalsy: true })
 //     .notEmpty()
-//     .withMessage('Please provide a valid email or username.'),
-//   check('password')
+//     .withMessage('Please provide a valid emaild')
 //     .exists({ checkFalsy: true })
 //     .withMessage('Please provide a password.'),
 //   handleValidationErrors
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { check } = require('express-validator');
+const sequelize = require('sequelize')
 const { handleValidationErrors } = require('../../utils/validation');
 
 const { Spot, Review } = require('../../db/models');
@@ -114,8 +115,14 @@ router.get('/current', requireAuth, async (req, res) => {
     let id = req.user.id;
 
     let result = await Spot.findAll({
-        where: { ownerId: id }
+        where: { ownerId: id },
+        include: { model: Image }
     })
+
+    // let result = await Spot.findAll({
+    //     where: { ownerId: id },
+    //     include: [model.Image]
+    // })
 
     res.json(result)
 })
