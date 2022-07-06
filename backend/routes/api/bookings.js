a2f83d691fc7a84c46530781e52f153630ad5509
Ben Durham <oorazugoku@gmail.com>
2022-07-06 14:30:05 -0500

    get all current user bookings done

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -4,6 +4,7 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { Booking, Spot, User } = require('../../db/models');
 
 
+// Reserve a Booking by Spot ID
 router.post('/spots/:spotId', requireAuth, async (req, res) => {
     let { startDate, endDate } = req.body
     const { id } = req.user
@@ -36,47 +37,55 @@ router.post('/spots/:spotId', requireAuth, async (req, res) => {
             if(startNum <= each.startDate.getTime() && endNum >= each.endDate.getTime()) {
                 check = true
                 proof = each
-            }
+            };
             if(startNum >= each.startDate.getTime() && startNum <= each.endDate.getTime()) {
                 check = true
                 proof = each
-            }
+            };
             if(endNum >= each.startDate.getTime() && endNum <= each.endDate.getTime()) {
                 check = true
                 proof = each
-            }
+            };
         });
     }
     if(check == true) {
-        res.status(401)
+        res.status(403)
         return res.json({
             message: `This Booking already exists inside Start and End date.`,
             proof
-        })
-    }
+        });
+    };
 
     const result = await Booking.create({
         userId: id,
         spotId: spotId,
         startDate: startDate,
         endDate: endDate
-    })
+    });
 
-    res.status(200)
+    res.status(200);
     res.json({
         message: `Resevation Successful!`,
         result
-    })
+    });
 });
 
 
-
-router.get('/', async (req, res) => {
-
-    const result = await Booking.findAll()
-
-    res.status(200)
-    res.json(result)
+// Get all Bookings by current User
+router.get('/current', requireAuth, async (req, res) => {
+    const { id } = req.user
+    const result = await Spot.findAll({
+        include: {
+            model: Booking,
+            where: { userId: id }
+        }
+    });
+    size = result.length
+    res.status(200);
+    res.json({
+        size,
+        result
+    });
 });
 
 
