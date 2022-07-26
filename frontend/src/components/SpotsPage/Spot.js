9b7aacaf66032e21e3961e3693be443162af7550
Ben Durham <oorazugoku@gmail.com>
2022-07-26 18:07:25 -0500

    spot progress

--- frontend/src/components/SpotsPage/Spot.js
+++ frontend/src/components/SpotsPage/Spot.js
@@ -30,15 +30,18 @@ function Spot() {
         <img src={house}/>
         </>
     )
-    
+
     return isLoaded && (
         <>
         <div className="spot-container">
             <div className="spot-header">
+                <div>
                 <h1>{spot.description}</h1>
-                <i className="fas fa-ghost" /> {spot.avgStarRating} · {spot.numReviews} {spot.numReviews === 1 ? 'Review' : 'Reviews'} · {spot.city}, {spot.state}, {spot.country}
+                <p/>
+                <i className="fas fa-ghost" /> {parseInt(spot.avgStarRating) == 0 ? 'New' : spot.avgStarRating} · {spot.numReviews} {spot.numReviews === 1 ? 'Review' : 'Reviews'} · {spot.city}, {spot.state}, {spot.country}
+                </div>
             {user && user.id === spot.Owner.id && (
-            <>
+            <div className="owner-spot-buttons">
                 <button
                 className="spot-edit-button"
                 onClick={() => setShowModal(true)}
@@ -51,18 +54,59 @@ function Spot() {
                 >
                     Delete Spot
                 </button>
-            </>
+            </div>
             )}
             </div>
-            <div type='button' className="spot-images">
-                {spot.Images.map(each => (<img key={each.id} src={each.url} />))}
-                {spot.Images.length < 1 && houseImg}
+            <div className="grid-container">
+                <div type='button' className="spot-images">
+                    {spot.Images.map((each, i) => (<div key={each.id} className={`spotImage${i + 1}`}><img src={each.url} /></div>))}
+                    {spot.Images.length < 1 && houseImg}
+                </div>
             </div>
+            <div className="spot-lowerInfo">
             <div className="spot-hostInfo">
                 {spot.name} hosted by {spot.Owner.firstName}
+                    <div className="spot-reviews">
+                    Reviews
+                    </div>
             </div>
-            <div className="spot-reviews">
-                Reviews
+            <div className="spot-booking">
+                <div className="spot-booking-price">
+                    <div>
+                    <span className='spot-booking-bigText'>${parseInt(spot.price)}</span> night
+                    </div>
+
+                        <div className="spot-booking-reviews">
+                            <i className="fas fa-ghost" />&nbsp;{parseInt(spot.avgStarRating) == 0 ? 'New' : spot.avgStarRating} · {spot.numReviews} {spot.numReviews === 1 ? 'Review' : 'Reviews'}
+                        </div>
+                </div>
+                <p />
+                <div className="spot-booking-price-breakdown">
+                    <p className="spot-booking-price-breakdown-text">You won't be charged yet</p>
+                    <div className="spot-booking-price-breakdown-left">
+                    ${parseInt(spot.price)} x 5 nights <p />
+                    Cleaning fee <p />
+                    Service fee <p />
+                    </div>
+                    <div className="spot-booking-price-breakdown-right">
+                    ${(parseInt(spot.price) * 5).toLocaleString()}<p />
+                    ${(((parseInt(spot.price) * 5) * .06).toFixed()).toLocaleString()} <p />
+                    ${(((parseInt(spot.price) * 5) * .1).toFixed()).toLocaleString()} <p />
+                    </div>
+                    <div className="spot-booking-price-breakdown-divider"></div>
+                </div>
+                    <div className="spot-booking-totals">
+                        <div>
+                            Total before taxes
+                        </div>
+                        <div className="spot-booking-totals2">
+                            ${(parseInt(parseInt(spot.price) * 5) + parseInt(((parseInt(spot.price) * 5) * .06).toFixed()) + parseInt(((parseInt(spot.price) * 5) * .1).toFixed())).toLocaleString()}
+                        </div>
+                    </div>
+
+
+
+            </div>
             </div>
         </div>
         {showModal && (
