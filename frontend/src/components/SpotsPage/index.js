34b69eab7431d5a5cd7dd43ca78f0eddbea41bb7
Ben Durham <oorazugoku@gmail.com>
2022-07-26 10:46:25 -0500

    added .toFixed to spots 2

--- frontend/src/components/SpotsPage/index.js
+++ frontend/src/components/SpotsPage/index.js
@@ -40,7 +40,7 @@ function SpotsPage() {
                 <p/>
                 Aug 1 - 7
                 <p/>
-                <b>${each.price.toFixed(0)}</b> night
+                <b>${each.price.toFixed()}</b> night
             </div>
         </div>
         ))}
